e8ce02b8d2579d0bfb8fa2d1d2fe5e2036fd5705
JonyM355 <joaoricardoam05@gmail.com>
2025-02-20 13:09:11 +0000

    Adding summaries

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// class Program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// função Main (principal)
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
