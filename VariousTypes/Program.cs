08ac48cae93a192b6cc703c978e7f4bf1fbc3630
JonyM355 <joaoricardoam05@gmail.com>
2025-02-20 13:28:06 +0000

    Adding 3 whole numbers and showing them in console

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n1 = 1;
+            int n2 = 159;
+            int n3 = 357;
+            Console.WriteLine($"{n1},\n{n2},\n{n3}");
+        }
+    }
+}
