e752d16250070555ae360f79433048b281bbde2c
JonyM355 <joaoricardoam05@gmail.com>
2025-02-21 16:30:44 +0000

    Convert a string to an int

---
+++ AskForInput/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ex.7
+
+            Console.WriteLine("Insert an int number");
+            int i = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Insert a real number");
+            double f = float.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine($"{i} plus {f} is equal to {i + f}");
+        }
+    }
+}
