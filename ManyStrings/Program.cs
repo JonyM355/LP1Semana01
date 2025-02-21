f074cdcb56cf0025d538a5fad1c79dacb3df738d
JonyM355 <joaoricardoam05@gmail.com>
2025-02-21 14:26:15 +0000

    Adding 3 string types and showing them on console

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string s1 = "Olá... \n...Adeus!";
+            string s2 = "Símbolo \u03A9!";
+            string s3 = "Tab \t !";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+        }
+    }
+}
