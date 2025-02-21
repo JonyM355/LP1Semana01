b7f2e496b4718347fc4a42ed27e6bc4baf660319
JonyM355 <joaoricardoam05@gmail.com>
2025-02-21 12:29:23 +0000

    Adding char variablese showing them on console

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -10,6 +10,11 @@ namespace VariousTypes
             int n2 = 159;
             int n3 = 357;
             Console.WriteLine($"{n1},\n{n2},\n{n3}");
+
+            char c1 = '\u2620';
+            char c2 = '\u24BB';
+            char c3 = '\u261F';
+            Console.WriteLine($"{c1},\n{c2},\n{c3}");
         }
     }
 }
