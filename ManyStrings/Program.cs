c716cc3fac54b992296c54ac905510c205913b68
JonyM355 <joaoricardoam05@gmail.com>
2025-02-21 15:53:29 +0000

    Format a double and an int

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -7,6 +7,12 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            // Ex.6*
+
+            double xx = 1.23456;
+            int ii = 19;
+
+
             // Ex.4
 
             string s1 = "Olá... \n...Adeus!";
@@ -30,6 +36,22 @@ namespace ManyStrings
             Console.WriteLine(si);
             Console.WriteLine(si1);
             Console.WriteLine(sc);
+
+
+            // Ex.6*
+
+            string xxf = $"xx = {xx:f2}";
+            string xxp = $"xx = {xx:p1}";
+
+            string iihx = $"ii = {ii:x}";
+            string iim = $"ii = {ii:c}";
+
+
+            Console.WriteLine(xxf);
+            Console.WriteLine(xxp);
+
+            Console.WriteLine(iihx);
+            Console.WriteLine(iim);
         }
     }
 }
