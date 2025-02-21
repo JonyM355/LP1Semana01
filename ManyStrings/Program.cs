2282b8a8b014b4bfcae92e605a174331654d0978
JonyM355 <joaoricardoam05@gmail.com>
2025-02-21 14:42:09 +0000

    Doing concatenation and interpolation with strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace ManyStrings
 {
@@ -6,6 +7,8 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            // Ex.4
+
             string s1 = "Olá... \n...Adeus!";
             string s2 = "Símbolo \u03A9!";
             string s3 = "Tab \t !";
@@ -13,6 +16,20 @@ namespace ManyStrings
             Console.WriteLine(s1);
             Console.WriteLine(s2);
             Console.WriteLine(s3);
+
+
+            // Ex.5
+
+            sbyte x = 10;
+            sbyte y = 36;
+            string si = $"{x} plus {y} is equal to {x + y}.";
+            string si1 = $"O valor de x é {x}";
+            string sc = "9" + x;
+
+
+            Console.WriteLine(si);
+            Console.WriteLine(si1);
+            Console.WriteLine(sc);
         }
     }
 }
