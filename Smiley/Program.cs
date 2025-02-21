b263540d441ecd13287950b73d65cda58d4851d4
JonyM355 <joaoricardoam05@gmail.com>
2025-02-21 17:58:30 +0000

    Tentativa do exercício prático

---
+++ Smiley/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número inteiro:");
+
+            int i = int.Parse(Console.ReadLine());
+
+            string s = $"{i:x}";
+
+            Console.WriteLine(s);
+
+            char c = Convert.ToChar(i);
+
+            
+            Console.WriteLine(c);
+        }
+    }
+}
